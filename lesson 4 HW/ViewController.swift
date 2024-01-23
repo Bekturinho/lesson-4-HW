b8dd5730b91d2bc378b9d95aa11d95468779e5b4
bekturinho <ismanalievbektur4@gmail.com>
2024-01-23 15:16:15 -0800

    Initial Commit

---
+++ lesson 4 HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson 4 HW
+//
+//  Created by fortune cookie on 1/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
